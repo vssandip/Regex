22478dbdc1e8d017a8df2bff0ed50b995674d4aa
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-12 12:06:43 +0530

    [ SANDIP ] Adding  'Welcome To User Registration'

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
